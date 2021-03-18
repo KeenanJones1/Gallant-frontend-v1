d0fa7edd639c9ec0c33f9bc94cf11882fa4d3821
Keenan Jones <keenanjones210@gmail.com>
2021-03-17 21:53:45 -0500

    Hello world

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  const handleForm = (e) => {
+    // returns string 
+    // format = 2021-03-17
+    console.log(typeof e.target[0].value)
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(event) => handleForm(event)}>
+        <input type="date" name="" id=""/>
+        <input type="submit" value="Enter"/>
+      </form>
     </div>
   );
 }
